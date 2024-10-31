5c8440fcf066437dde8f43d7ce09f8dcde898e48
Orangunio <miloszdubowski@wp.pl>
2024-10-31 20:26:10 +0100

    Student View + student/rodzic/nauczyciel w bazie

--- Controller/StudentController.cs
+++ Controller/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElektronicznyKonsolowy.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,15 @@ namespace ElektronicznyKonsolowy.Controller
 {
     public class StudentController
     {
+        StudentView studentView = new StudentView();
         public StudentController() { }
-        public void Run() { }
+        public void Run() 
+        {
+            bool run = true; int choose;
+            while (run)
+            {
+                choose = studentView.ShowMainMenu();
+            }
+        }
     }
 }
---
+++ View/StudentView.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronicznyKonsolowy.View
+{
+    public class StudentView
+    {
+        public StudentView() { }
+        public int ShowMainMenu()
+        {
+            var options = new[]
+            {
+                "Edytuj swoje dane",
+                "Przegladaj swoje oceny",
+                "Wyloguj"
+            };
+            var selectedOption =AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Co chcesz wykonać?")
+                    .PageSize(10)
+                    .AddChoices(options));
+
+            return Array.IndexOf(options, selectedOption);
+        }
+    }
+}
