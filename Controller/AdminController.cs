c016d2f2b39e4c90c1d0bbd0c32612bb3dbd23b3
MaRchello28 <167532593+MaRchello28@users.noreply.github.com>
2024-10-31 19:42:15 +0100

    Menu głowne Admina Wybory

--- Controller/AdminController.cs
+++ Controller/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using ElektronicznyKonsolowy.Models;
+using ElektronicznyKonsolowy.View;
 using Spectre.Console;
 using System;
 using System.Collections.Generic;
@@ -10,66 +11,18 @@ namespace ElektronicznyKonsolowy.Controller
 {
     public class AdminController
     {
+        AdminView adminView = new AdminView();
         public AdminController() 
         { 
             
         }
         public void Run() 
         {
-            bool run = true;
+            bool run = true; int choose;
             while(run)
             {
-                var fruit = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                .Title("Co chcesz wykonać?")
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-                .AddChoices(new[] {
-                    "Zarzadzanie kontami uczniow",
-                    "Zarzadzanie kontami nauczycieli",
-                    "Zarzadzanie kontami rodzicow",
-                    "Zarzadzanie klasami",
-                    "Przypisz uczniow do odpowieniej klasy",
-                    "Zarzadzaj przedmiotami",
-                    "Przypisz przedmioty do klasy"
-                }));
+                choose = adminView.ShowMainMenu();
             }
         }
-        /*public Student CreateStudent()
-        {
-
-        }
-        public Student UpdateStudent()
-        {
-
-        }
-        public void DeleteStudent()
-        {
-
-        }
-        public Teacher CreateTeacher()
-        {
-
-        }
-        public Teacher UpdateTeacher()
-        {
-
-        }
-        public void DeleteTeacher()
-        {
-
-        }
-        public Parent CreateParent()
-        {
-
-        }
-        public Parent UpdateParent()
-        {
-
-        }
-        public void DeleteParent()
-        {
-
-        }*/
     }
 }
---
+++ View/AdminView.cs
@@ -0,0 +1,37 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronicznyKonsolowy.View
+{
+    public class AdminView
+    {
+        public AdminView() 
+        { 
+            
+        }
+        public int ShowMainMenu()
+        {
+            var options = new[] {
+            "Zarządzanie kontami uczniów",
+            "Zarządzanie kontami nauczycieli",
+            "Zarządzanie kontami rodziców",
+            "Zarządzanie klasami",
+            "Przypisz uczniów do odpowiedniej klasy",
+            "Zarządzaj przedmiotami",
+            "Przypisz przedmioty do klasy",
+            "Wyloguj"
+    };
+            var selectedOption = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Co chcesz wykonać?")
+                    .PageSize(10)
+                    .AddChoices(options));
+
+            return Array.IndexOf(options, selectedOption);
+        }
+    }
+}
