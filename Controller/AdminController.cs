1a1c584b8893d8ce504298356ad5f1c6ca22e075
MaRchello28 <167532593+MaRchello28@users.noreply.github.com>
2024-10-31 19:27:41 +0100

    Okno menu główne Admina

--- Controller/AdminController.cs
+++ Controller/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using ElektronicznyKonsolowy.Models;
+using Spectre.Console;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,27 @@ namespace ElektronicznyKonsolowy.Controller
         { 
             
         }
-        public void Run() { }
+        public void Run() 
+        {
+            bool run = true;
+            while(run)
+            {
+                var fruit = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Co chcesz wykonać?")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                .AddChoices(new[] {
+                    "Zarzadzanie kontami uczniow",
+                    "Zarzadzanie kontami nauczycieli",
+                    "Zarzadzanie kontami rodzicow",
+                    "Zarzadzanie klasami",
+                    "Przypisz uczniow do odpowieniej klasy",
+                    "Zarzadzaj przedmiotami",
+                    "Przypisz przedmioty do klasy"
+                }));
+            }
+        }
         /*public Student CreateStudent()
         {
 
